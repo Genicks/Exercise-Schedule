d4ded2898e7b816eb3ad8e2bff0b05051ec3a766
Genick's <genicks.html@gmail.com>
2023-12-10 20:09:19 -0400

    Updtated JSON Data base editing

--- fe/src/components/Config.jsx
+++ fe/src/components/Config.jsx
@@ -1,13 +1,28 @@
-import axios from "axios"
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import hljs from "highlight.js";
+import "highlight.js/styles/default.css";
+
+const LinesOfCode = (props) => {
+  const { lines, classC } = props;
+
+  return (
+    <div>
+      {lines.map((num, i) => (
+        <div key={i} className={`linesOfCode ${classC}`}>
+          <p>{i + 1}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const Config = (props) => {
   const { data } = props;
-  const fetchedData = JSON.stringify(data, null, 2);
+  const fetchedData = JSON.stringify(data, null, 3);
   const [textAreaContent, setTextAreaContent] = useState("");
   const [isButtonDisabled, setButtonDisabled] = useState(true);
 
-  
   useEffect(() => {
     setTextAreaContent(fetchedData);
     setButtonDisabled(true);
@@ -17,37 +32,70 @@ const Config = (props) => {
     setTextAreaContent(event.target.value);
     setButtonDisabled(false);
   };
-  
+
   const handleButtonClick = () => {
-    axios.post('http://localhost:4000/config', { textAreaContent })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
+    axios
+      .post("http://localhost:4000/config", { textAreaContent })
+      .then(() => {
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
-  
+
+  function countLines(str) {
+    const linesArray = str.split(/\r\n|\r|\n/);
+    return linesArray;
+  }
+
+  const lines = countLines(fetchedData);
+
   //! TEST ------
-    // console.log(data)
-    // console.log(fetchedData)
-    // console.log(textAreaContent)
+  // console.log(data)
+  // console.log(fetchedData)
+  // console.log(textAreaContent)
   //! TEST ------
   return (
     <div>
-      <textarea
-        name="config"
-        id="config"
-        value={textAreaContent}
-        onChange={handleTextAreaChange}
-      />
-      <button
-        disabled={isButtonDisabled}
-        className="btn"
-        onClick={handleButtonClick}
-      >
-        Update Json
-      </button>
+      <h1 className="header header1"> View JSON Data</h1>
+      <div className="codeCon">
+        <div className="codeViewer">
+          <LinesOfCode lines={lines} />
+          <pre className="Code">
+            <code
+              className="language-JSON"
+              dangerouslySetInnerHTML={{
+                __html: hljs.highlight("json", textAreaContent).value,
+              }}
+            />
+          </pre>
+        </div>
+      </div>
+
+      <h1 className="header"> Edit Raw JSON Data</h1>
+
+      <div className="btnCon">
+        <button
+          disabled={isButtonDisabled}
+          className="btn"
+          onClick={handleButtonClick}
+        >
+          Update Json
+        </button>
+      </div>
+
+      <div className="codeCon">
+        <div className="codeEditor">
+          <LinesOfCode lines={lines} classC="codeEditorLines" />
+          <textarea
+            name="config"
+            id="config"
+            value={textAreaContent}
+            onChange={handleTextAreaChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
